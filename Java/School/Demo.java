18e71697be78bab9a652f0b8f42b944a65bb491f
LlamaAdam <da11amninja@gmail.com>
2020-07-21 00:44:54 -0500

    Java Work

---
+++ Java/School/Demo.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Demo {
+   public static void main(String args[]) {
+      int num1 = 10;
+      int num2 = 20;
+
+      num2 = num1;
+      System.out.println("= Output: "+num2);
+
+      num2 += num1;
+      System.out.println("+= Output: "+num2);
+
+      num2 -= num1;
+      System.out.println("-= Output: "+num2);
+
+      num2 *= num1;
+      System.out.println("*= Output: "+num2);
+
+      num2 /= num1;
+      System.out.println("/= Output: "+num2);
+
+      num2 %= num1;
+      System.out.println("%= Output: "+num2);
+   }
+}
